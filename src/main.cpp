3d00e0821fa1da053c3946bc8e4711438994ea04
marcel <marcel.hajek@gmx.de>
2025-09-26 01:44:38 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,15 @@
 // main.cpp
 
-#include "content.h"
-#include "core_utils.h"
 #include "defer.h"
 #include "keyword_binding.h"
+#include "project_gen.h"
 
-#include <utility>
-
-static auto generate_project(const int argc,
-                             const char *const *const argv) noexcept -> void {
-  const auto project_name_result = get_project_name(argc, argv);
-
-  if (!project_name_result) {
-    std::println("{}", project_name_result.error());
-    std::terminate();
-  }
-  const auto project_name = project_name_result.value();
-
-  std::println("creating directories...");
-
-  const auto project_path_result = make_project_directory(project_name);
-
-  if (!project_path_result) {
-    std::println("{}", project_path_result.error());
-    std::terminate();
-  }
-  const auto project_directory = project_path_result.value();
-
-  std::println("project_directory: {}", project_directory.string());
-  const auto src_directory_result = make_src_directory(project_directory);
-
-  if (!src_directory_result) {
-    std::println("{}", project_path_result.error());
-    std::terminate();
-  }
-  const auto src_directory = src_directory_result.value();
-
-  std::println("src_directory: {}", src_directory.string());
-
-  const auto static_files = {File<std::string_view>{"main.cpp", src_directory,
-                                                    content::get_main_cpp()}};
-
-  const auto dynamic_files = {
-      File<std::string>{"CMakeLists.txt", project_directory,
-                        content::get_cmake_lists_txt(project_name)}};
-
-  std::println("\ncreating files...");
-
-  const auto write_static_files_result =
-      write_files<std::string_view>(std::move(static_files));
-
-  if (!write_static_files_result) {
-    std::println("{}", write_static_files_result.error());
-    std::terminate();
-  }
-
-  const auto write_dynamic_files_result =
-      write_files<std::string>(std::move(dynamic_files));
-
-  if (!write_dynamic_files_result) {
-    std::println("{}", write_dynamic_files_result.error());
-    std::terminate();
-  }
-}
+#include <print>
 
 auto main(const int argc, const char *const *const argv) noexcept -> int {
-  if (argc < MIN_ARGS_TO_GENERATE_PROJECT_NAME) {
+  if (argc < core_utils::MIN_ARGS_TO_GENERATE_PROJECT_NAME) {
     std::println("{} args are required!\n",
-                 MIN_ARGS_TO_GENERATE_PROJECT_NAME - 1);
+                 core_utils::MIN_ARGS_TO_GENERATE_PROJECT_NAME - 1);
     for (std::uint8_t i = 0;
          const KeywordBinding &keyword_binding :
          std::initializer_list{KeywordBinding{"proj", [&]() {}, "my_proj_name"},
---
+++ src/project_gen.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "content.h"
+#include "core_utils.h"
+
+#include <print>
+
+static auto generate_project(const int argc,
+                             const char *const *const argv) noexcept -> void {
+  const auto project_name_result =
+      core_utils::CoreUtils::get_project_name(argc, argv);
+
+  if (!project_name_result) {
+    std::println("{}", project_name_result.error());
+    std::terminate();
+  }
+  const auto project_name = project_name_result.value();
+
+  std::println("creating directories...");
+
+  const auto project_path_result =
+      core_utils::CoreUtils::make_project_directory(project_name);
+
+  if (!project_path_result) {
+    std::println("{}", project_path_result.error());
+    std::terminate();
+  }
+  const auto project_directory = project_path_result.value();
+
+  std::println("project_directory: {}", project_directory.string());
+  const auto src_directory_result =
+      core_utils::CoreUtils::make_src_directory(project_directory);
+
+  if (!src_directory_result) {
+    std::println("{}", project_path_result.error());
+    std::terminate();
+  }
+  const auto src_directory = src_directory_result.value();
+
+  std::println("src_directory: {}", src_directory.string());
+
+  const auto static_files = {core_utils::File<std::string_view>{
+      "main.cpp", src_directory, content::get_main_cpp()}};
+
+  const auto dynamic_files = {core_utils::File<std::string>{
+      "CMakeLists.txt", project_directory,
+      content::get_cmake_lists_txt(project_name)}};
+
+  std::println("\ncreating files...");
+
+  const auto write_static_files_result =
+      core_utils::CoreUtils::write_files<std::string_view>(
+          std::move(static_files));
+
+  if (!write_static_files_result) {
+    std::println("{}", write_static_files_result.error());
+    std::terminate();
+  }
+
+  const auto write_dynamic_files_result =
+      core_utils::CoreUtils::write_files<std::string>(std::move(dynamic_files));
+
+  if (!write_dynamic_files_result) {
+    std::println("{}", write_dynamic_files_result.error());
+    std::terminate();
+  }
+}
