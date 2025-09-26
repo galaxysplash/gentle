51449534df307219849429512f4a9b45a7c307dd
marcel <marcel.hajek@gmx.de>
2025-09-26 21:06:06 +0200

    ...

--- src/content.cpp
+++ src/content.cpp
@@ -61,3 +61,34 @@ content::ModuleGen::get_mod_h(const std::string_view &module_name,
          "};\n";
   return ret;
 }
+
+[[nodiscard]] auto
+content::ModuleGen::get_mod_cpp(const std::string_view &module_name,
+                                const std::string_view &header_name) noexcept
+    -> std::string {
+  std::string ret;
+
+  ret = "// ";
+  ret += header_name;
+  ret += ".cpp\n\n";
+
+  ret += "#include \"";
+  ret += header_name;
+  ret += ".h\"\n\n";
+  ret += "auto ";
+  ret += module_name;
+  ret += "::run() noexcept -> void {\n"
+         "  // insert code here...\n"
+         "}\n";
+  return ret;
+}
+
+[[nodiscard]] auto content::ModuleGen::get_module_cmake_lists_txt(
+    const std::string_view &name) noexcept -> std::string {
+  std::string ret;
+
+  ret += Base::get_cmake_lists_txt(name);
+  ret += "add_executable(${PROJECT_NAME} ${SOURCES})\n";
+
+  return ret;
+}
--- src/content.h
+++ src/content.h
@@ -27,33 +27,10 @@ struct ModuleGen {
 
   [[nodiscard]] static auto
   get_mod_cpp(const std::string_view &module_name,
-              const std::string_view &header_name) noexcept -> std::string {
-    std::string ret;
-
-    ret = "// ";
-    ret += header_name;
-    ret += ".cpp\n\n";
-
-    ret += "#include \"";
-    ret += header_name;
-    ret += ".h\"\n\n";
-    ret += "auto ";
-    ret += module_name;
-    ret += "::run() noexcept -> void {\n"
-           "  // insert code here...\n"
-           "}\n";
-    return ret;
-  }
+              const std::string_view &header_name) noexcept -> std::string;
 
   [[nodiscard]] static auto
   get_module_cmake_lists_txt(const std::string_view &name) noexcept
-      -> std::string {
-    std::string ret;
-
-    ret += Base::get_cmake_lists_txt(name);
-    ret += "add_executable(${PROJECT_NAME} ${SOURCES})\n";
-
-    return ret;
-  }
+      -> std::string;
 };
 } // namespace content
