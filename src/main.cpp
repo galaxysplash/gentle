d5feb54fbee53aabfe7c3bfc74b44707516020cd
marcel <marcel.hajek@gmx.de>
2025-09-26 03:00:09 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,22 @@
 #include <string_view>
 
 auto main(const int argc, const char *const *const argv) noexcept -> int {
-  auto keyword_bindings = {
-      KeywordBinding{"proj",
-                     [&argc, &argv]() -> std::expected<void, std::string_view> {
-                       return generate_project(argc, argv);
-                     },
-                     "my_proj_name"},
-      KeywordBinding{"mod",
-                     [&argc, &argv]() -> std::expected<void, std::string_view> {
-                       return generate_module(argc, argv);
-                     },
-                     "my_mod_name"}};
-
-  const auto match_keyword_result =
-      match_keyword(argc, argv, std::move(keyword_bindings));
-
-  if (!match_keyword_result) {
+  if (const auto match_keyword_result = match_keyword(
+          argc, argv,
+          {KeywordBinding{
+               "proj",
+               [&argc, &argv]() -> std::expected<void, std::string_view> {
+                 return generate_project(argc, argv);
+               },
+               "my_proj_name"},
+           KeywordBinding{
+               "mod",
+               [&argc, &argv]() -> std::expected<void, std::string_view> {
+                 return generate_module(argc, argv);
+               },
+               "my_mod_name"}});
+      !match_keyword_result) {
     std::println("{}", match_keyword_result.error());
-    std::terminate();
+    return -1;
   }
 }
