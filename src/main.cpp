e429342a7cf509b1ce307d77b0b13974ffb436d3
marcel <marcel.hajek@gmx.de>
2025-09-27 10:52:03 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -42,17 +42,17 @@ auto main(const int argc, const char *const *const argv) noexcept -> int {
           KeywordBinding{
               "run",
               [&argc, &argv]() noexcept -> std::expected<void, std::string> {
-                if (!std::system("cmake . -B build")) {
+                if (std::system("cmake . -B build")) {
                   return std::unexpected{"cmake generation command failed."};
                 }
                 if (const auto make_build_cmd = "make -C build";
-                    !std::system(make_build_cmd)) {
+                    std::system(make_build_cmd)) {
                   std::println("make build command \"{}\" failed. falling back "
                                "to ninja",
                                make_build_cmd);
 
                   if (const auto ninja_build_cmd = "ninja -C build";
-                      !std::system(ninja_build_cmd)) {
+                      std::system(ninja_build_cmd)) {
                     return std::unexpected{
                         std::format("make's and ninja's build commands failed "
                                     "failed.\n\nmake cmd: (\"{}\")\n,\n ninja "
@@ -68,7 +68,7 @@ auto main(const int argc, const char *const *const argv) noexcept -> int {
                 const auto run_name = run_name_result.value();
                 const auto run_cmd = std::format("./build/{}", run_name);
                 if (const auto &run_cmd_cstr = run_cmd.c_str();
-                    !std::system(run_cmd_cstr)) {
+                    std::system(run_cmd_cstr)) {
                   return std::unexpected{std::format(
                       "run command (\"{}\") failed.", run_cmd_cstr)};
                 }
