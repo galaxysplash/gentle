63669c71771c25d726f092ca071fc2c0b21668b8
marcel <marcel@marcels-MacBook-Pro.fritz.box>
2025-09-27 18:41:43 +0200

    ...

--- src/keyword_binding.h
+++ src/keyword_binding.h
@@ -6,12 +6,12 @@
 #include <functional>
 #include <string_view>
 
-struct KeywordBinding {
+struct KeywordBinding final {
   explicit inline KeywordBinding(
       const std::string_view keyword_name,
       std::function<std::expected<void, std::string>()> &&lambda) noexcept
       : keyword_name(keyword_name), lambda(lambda) {}
-
+  KeywordBinding(const KeywordBinding &) = delete;
   const std::string_view keyword_name;
   const std::function<std::expected<void, std::string>()> lambda;
 };
