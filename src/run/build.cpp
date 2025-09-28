909795f8347b59956dd5b4977abe67b6f2fbd362
marcel <marcel.hajek@gmx.de>
2025-09-28 18:03:04 +0200

    ...

--- src/run/build.cpp
+++ src/run/build.cpp
@@ -2,7 +2,28 @@
 
 #include "build.h"
 
+#include <format>
+#include <print>
+
 auto Build::run() noexcept -> std::expected<void, std::string> {
-  // insert code here...
+  if (std::system("cmake . -B build")) {
+    return std::unexpected{"cmake generation command failed."};
+  }
+  if (const auto make_build_cmd = "make -C build";
+      std::system(make_build_cmd)) {
+    std::println("make build command \"{}\" failed. falling back "
+                 "to ninja",
+                 make_build_cmd);
+
+    if (const auto ninja_build_cmd = "ninja -C build";
+        std::system(ninja_build_cmd)) {
+      return std::unexpected{
+          std::format("make's and ninja's build commands failed "
+                      "failed.\n\nmake cmd: (\"{}\")\n,\n ninja "
+                      "build cmd: (\"{}\")",
+                      make_build_cmd, ninja_build_cmd)};
+    }
+  }
+
   return {};
 }
--- src/run/run.cpp
+++ src/run/run.cpp
@@ -1,36 +1,20 @@
 // run.cpp
 
 #include "core_utils.h"
+#include "run/build.h"
 
 #include <expected>
 #include <filesystem>
 #include <format>
-#include <print>
 #include <string>
 
 #include "run.h"
 
 auto Run::run(const int argc, const char *const *const argv) noexcept
     -> std::expected<void, std::string> {
-  if (std::system("cmake . -B build")) {
-    return std::unexpected{"cmake generation command failed."};
+  if (const auto result = Build::run(); !result) {
+    std::unexpected{result.error()};
   }
-  if (const auto make_build_cmd = "make -C build";
-      std::system(make_build_cmd)) {
-    std::println("make build command \"{}\" failed. falling back "
-                 "to ninja",
-                 make_build_cmd);
-
-    if (const auto ninja_build_cmd = "ninja -C build";
-        std::system(ninja_build_cmd)) {
-      return std::unexpected{
-          std::format("make's and ninja's build commands failed "
-                      "failed.\n\nmake cmd: (\"{}\")\n,\n ninja "
-                      "build cmd: (\"{}\")",
-                      make_build_cmd, ninja_build_cmd)};
-    }
-  }
-
   auto run_name = std::string{};
 
   if (argc == 3) [[unlikely]] {
