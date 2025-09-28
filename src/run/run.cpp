f77f4775ec58ed348e000b9cac86ec19b64d734b
marcel <marcel.hajek@gmx.de>
2025-09-28 18:00:36 +0200

    ...

--- src/run/run.cpp
+++ src/run/run.cpp
@@ -2,8 +2,11 @@
 
 #include "core_utils.h"
 
+#include <expected>
+#include <filesystem>
 #include <format>
 #include <print>
+#include <string>
 
 #include "run.h"
 
@@ -27,11 +30,22 @@ auto Run::run(const int argc, const char *const *const argv) noexcept
                       make_build_cmd, ninja_build_cmd)};
     }
   }
-  const auto run_name_result = core_utils::CoreUtils::get_name(argc, argv);
-  if (!run_name_result) {
-    return std::unexpected{run_name_result.error()};
+
+  auto run_name = std::string{};
+
+  if (argc == 3) [[unlikely]] {
+    const auto run_name_result = core_utils::CoreUtils::get_name(argc, argv);
+    if (!run_name_result) {
+      return std::unexpected{run_name_result.error()};
+    }
+    run_name = run_name_result.value();
+  } else if (argc == 2) [[likely]] {
+    run_name = std::filesystem::current_path().filename();
+  } else if (argc < 2) [[unlikely]] {
+    return std::unexpected{"to less args.\nwanted: \"gentle run\""};
+  } else if (argc > 3) [[unlikely]] {
+    return std::unexpected{"too much args.\nwanted: \"gentle run\""};
   }
-  const auto run_name = run_name_result.value();
   const auto run_cmd = std::format("./build/{}", run_name);
   if (const auto &run_cmd_cstr = run_cmd.c_str(); std::system(run_cmd_cstr)) {
     return std::unexpected{
