a706817a4ea559ebda1c373e4c7297e8c527d079
marcel <marcel.hajek@gmx.de>
2025-10-01 04:07:24 +0200

    ...

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # gentle
+
+## general
+MANPAGE:
+gentle build => to build your project
+
+gentle run => to run your project
+
+gentle my_project => to create a new project, where 'my_project' is the name
+
+gentle-- help => to get to this 'help' screen
+
+gentle class my_class_name => to create a class, where 'my_class_name' is the name
+
+gentle new my_project_name => to create a project, where 'my_project_name' is the name 
+
+gentle mod my_mod_name => to create a class, where 'my_class_name' is the name
+
+NOTE:
+You can use 'gentle --help' to list them up.
+
+## prerequisites
+cmake
+make/ninja, but way better make
+a c++ 23 compatible compiler
+
+## build
+build it like any other cmake project
--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,35 @@
 
 #include <expected>
 #include <format>
+#include <iostream>
 #include <print>
 #include <string>
 #include <string_view>
 
 auto main(const int argc, const char *const *const argv) -> int {
-  if (argc == 2) {
-    if (std::string_view{argv[1]} == "run") [[likely]] {
+  if (argc == 2) { // a welded on if chain
+    if (std::string_view{argv[1]} == "--help" ||
+        std::string_view{argv[1]} == "-h" ||
+        std::string_view{argv[1]} == "help") [[unlikely]] {
+      std::cout << R"(MANPAGE:
+gentle build => to build your project
+
+gentle run => to run your project
+
+gentle my_project => to create a new project, where 'my_project' is the name
+
+gentle-- help => to get to this 'help' screen
+
+gentle class my_class_name => to create a class, where 'my_class_name' is the name
+
+gentle new my_project_name => to create a project, where 'my_project_name' is the name 
+
+gentle mod my_mod_name => to create a class, where 'my_mod_name' is the name
+
+gentle sub my_sub_name => to create a class, where 'my_sub_name' is the name
+)";
+      return 0;
+    } else if (std::string_view{argv[1]} == "run") [[likely]] {
       if (const auto result = Run::run(argc, argv); !result) {
         std::println("{}", result.error());
         return -1;
