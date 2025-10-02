310fd6097f3c5db90b4601686f9248ae0de84ad6
marcel <marcel.hajek@gmx.de>
2025-10-02 23:32:20 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -131,8 +131,10 @@ auto main() -> int {
            project_path / core_utils::INCLUDE_DIRECTORY_NAME / project_name,
            content::ProjGen::get_custom_main_h(R"(#include <cstdint>
 
-extern "C" auto asm_add(const std::uint64_t lhs, const std::uint64_t rhs) noexcept 
+extern "C" {
+auto asm_add(const std::uint64_t lhs, const std::uint64_t rhs) noexcept
     -> std::uint8_t;
+}
 )"),
        },
        core_utils::File<std::string>{"main.asm", src_path, R"(global asm_add
