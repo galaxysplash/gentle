2159f0cef6dca7bee943e473037666fc91b1394e
marcel <marcel.hajek@gmx.de>
2025-10-04 01:24:17 +0200

    ...

--- run.sh
+++ run.sh
@@ -4,12 +4,19 @@ cmake . -B build
 make -C build
 mkdir tests
 cd tests
-echo ""
-echo ""
-./../build/gentle proj test_project
-echo ""
-echo ""
-cd test_project
-./../../build/gentle mod hey
-./../../build/gentle sub my_sub
-./../../build/gentle run
+
+test_gentle() {
+  echo ""
+  echo ""
+  ./../build/gentle $1 $2
+  echo ""
+  echo ""
+  cd $2
+  ./../../build/gentle mod hey
+  ./../../build/gentle sub my_sub
+  ./../../build/gentle run
+}
+
+test_gentle(proj test_project)
+test_gentle(asm asm_proj)
+
