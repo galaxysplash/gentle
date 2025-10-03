f2f778a65c29a8f17a1b9a91c2c6b6c72e777e41
marcel <marcel.hajek@gmx.de>
2025-10-04 01:37:40 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -285,5 +285,20 @@ auto main(const int argc, const char *const *const argv) -> int {
   if (!match_keyword_result) [[unlikely]] {
     std::print("{}\n", match_keyword_result.error());
     return -1;
+  } else [[likely]] {
+    const auto project_name_result =
+        core_utils::CoreUtils::get_name(argc, argv);
+
+    if (!project_name_result) {
+      std::print("could not get the project name planed,\n"
+                 "for using in teh 'cd' instruction\n");
+      return -5;
+    }
+    const auto project_name = project_name_result.value();
+    if (const auto instruction = std::format("cd {}", project_name);
+        system(instruction.c_str()) != 0) {
+      std::print("'{}' din't work.\n", instruction);
+      return -10;
+    }
   }
 }
