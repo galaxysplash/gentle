3821cff8ae032246a5ba838c061b820fb192c37a
marcel <Marcel.Hajek@gmx.de>
2025-10-04 22:55:05 +0200

    ...

--- src/generate/generate_lang_proj.cpp
+++
@@ -1,8 +0,0 @@
-// generate_lang_proj.cpp
-
-#include "generate_lang_proj.h"
-
-auto GenerateLangProj::run() noexcept -> std::expected<void, std::string> {
-  // insert code here...
-  return {};
-}
